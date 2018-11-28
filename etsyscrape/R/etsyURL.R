c0a56469b82d38f99deb9c5b8b5e7020da91dade
Meagan McCardle <meagan_mccardle@brown.edu>
2018-11-28 18:16:43 -0500

    functioning r package with etsyURL function :)

---
+++ etsyscrape/R/etsyURL.R
@@ -0,0 +1,31 @@
+#'
+#' Function to create Etsy.com URL for item search.
+#'
+#' This function creates the URL associated with an Etsy.com search for a specific item.
+#'
+#' @param search_string What item would you like to search for?
+#'
+#' @param search_order Specify the order in which the search results should be returned.
+#'     Default is "most_relevent".
+#'     Other options include "date_desc", "price_desc", "price_asc".
+#'
+#' @keywords url
+#'
+#' @return Returns an Etsy.com URL corresponding to a search for the queried item.
+#'
+#' @examples
+#' etsyURL("knitted scarf")
+#' etsyURL("welcome sign", "date_desc")
+#'
+#' @export
+
+etsyURL <- function(search_string, search_order='most_relevant') {
+  base_url='https://www.etsy.com/search?q='
+  search_string <- gsub(' ', '+', search_string, fixed = TRUE)
+  # Other search options -- most recent: date_desc, highest price: price_desc, lowest price: price_asc.
+  search_order <- paste0('&explicit=1&order=', search_order)
+  url <- paste0(base_url, search_string, search_order)
+  return(url)
+}
+
+
---
+++ etsyscrape/R/etsyscrape.R
@@ -0,0 +1,24 @@
+#' etsyscrape: Etsy Web Scraping Tools
+#'
+#' This packages allows the user to examine search results scraped from Etsy.com.
+#' The package contains functions to build an Etsy URL based on a search string and desired ordering
+#' of results, and functions to gather information about the names of the items, the shops they are sold
+#' in, the location of the shop, and links to the items themselves.
+#'
+#' @section etsyscrape functions:
+#' \itemize{
+#'   \item etsyURL
+#'   \item etsyXML
+#'   \item etsyItemResults
+#'   \item etsyItemLink
+#'   \item etsyShopResults
+#'   \item etsyShopLocation
+#' }
+#'
+#'
+#' @author Meagan McCardle \email{meagan_mccardle@brown.edu}
+#' @author Nana Boame \email{nana_boame@brown.edu}
+#'
+#' @docType package
+#' @name etsyscrape
+NULL
