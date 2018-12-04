3e72feb39deca3173b77036f49bb976040fb0b31
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-04 18:28:11 -0500

    consistency update and changing internal functions to new names

--- etsyscrape/R/etsyShopReviews.R
+++ etsyscrape/R/etsyShopReviews.R
@@ -1,20 +1,23 @@
 #'
-#' Function to obtain shop reviews.
+#' Function to obtain the customer text-based reviews of each shop selling an item returned by an Etsy.com search.
 #'
-#' This function scrapes the text-based customer reviews of each shop resulting from the etsy.com search
-#' The function uses the etsyGetShops function to extract the names of the relevent etsy shops.
-#' It then creates a url for each individual shop, and scrapes that URL for the shop reviews.
-#' The function collapses all the different reviews on the first page (only) of the shop's review url into one text paragraph,
-#' and returns a vector of paragraphs representing the reviews from each shop.
+#' This function scrapes the customer reviews of each Etsy shop.
+#' \itemize{
+#'   \item Utilizes the etsyShopResults() function to extract the name of each Etsy shop from the
+#'   Etsy.com search results.
+#'   \item Creates a URL for each individual shop, and scrapes the shop URL for the shop reviews.
+#'   \item Collapses each individual review from the first page of the shop's reviews (only)
+#'   into one text paragraph.
+#' }
 #'
 #' @param results_xml An xml object, obtained from scraping an Etsy search results webpage
-#'
 #'
 #' @keywords html
 #' @keywords xml
 #' @keywords reviews
+#' @keywords shop
 #'
-#' @return Returns the shop reviews as a character vector.
+#' @return Returns a character vector of paragraphs representing the reviews from each individual shop.
 #'
 #' @example
 #' etsyShopReviews(results_xml)
@@ -22,13 +25,13 @@
 #' @export
 
 etsyShopReviews <- function(results_xml) {
-  shop <- etsyGetShops(results_xml)
+  shop <- etsyShopResults(results_xml)
   reviews <- vector(length = 0)
 
   for (i in 1:length(shop)) {
     reviews_url <- paste("https://www.etsy.com/shop/", shop[i], "/reviews", sep="")
-    page <- read_html(reviews_url)
-    review_text <- paste(html_text(html_nodes(page, ".m-xs-0")), collapse = " ", sep=" ")
+    page <- xml2::read_html(reviews_url)
+    review_text <- paste(rvest::html_text(rvest::html_nodes(page, ".m-xs-0")), collapse =" ", sep=" ")
     reviews <- c(reviews, review_text)
   }
   return(reviews)
