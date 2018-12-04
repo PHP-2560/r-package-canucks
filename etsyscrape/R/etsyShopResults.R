41f0f7ba0233706f1abd4e188287404a8bd37d59
nboame <43015143+nboame@users.noreply.github.com>
2018-12-04 13:26:37 -0500

    function to obtain shop name
    unsure if need to include loading additional packages in code

---
+++ etsyscrape/R/etsyShopResults.R
@@ -0,0 +1,23 @@
+#'
+#' Function to obtain shop name.
+#'
+#' @param results_xml An xml object, obtained from scraping an Etsy search results webpage.
+#'
+#' @keywords html
+#' @keywords xml
+#' @keywords shops
+#'
+#' @return Returns the shop name as a character vector. 
+#'
+#' @example 
+#' etsyShopResults(results_xml)
+#'
+#' @export
+
+library(rvest)
+library(tidyverse)
+
+etsyShopResults <- function(results_xml) {
+  shops <- trimws(html_text(html_nodes(results_xml, "p.text-gray-lighter")), which = "both")
+  return(shops)
+}
